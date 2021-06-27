3b2aec252a897adb7e3c1f84ff1369578b5fb040
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-06-27 16:00:59 +0100

    add the flipper color function and append to background

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+//select the html elements
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+//eventListener to btn
+
+btn.addEventListener('click', function(){
+    const randomNumber = Math.floor(Math.random() * colors.length)
+    document.body.style.backgroundColor = colors[randomNumber]
+})
